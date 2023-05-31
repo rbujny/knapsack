20d07f9862f3c55e6ddc7f3309609c04e1e3f67b
rbujny <radoslaw.bujny@gmail.com>
2023-05-31 08:36:37 +0200

    Final

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ while error:
             V.append(int(line.split(" ")[1]))
 
 
+# siłowy
 def bfAlgorithm(n, b, d):
     fmax = 0
     resSize = 0
@@ -69,7 +70,7 @@ def bfAlgorithm(n, b, d):
                 num = abs(j - n)
                 rx += d[num]["r"]
                 wx += d[num]["w"]
-                elements.append(num)
+                elements.append(num - 1)
         if rx <= b:
             if wx > fmax:
                 fmax = wx
@@ -78,54 +79,82 @@ def bfAlgorithm(n, b, d):
     return [elmax[::-1], resSize, fmax]
 
 
-def dyAlgorithm(c, n, W, V):
-    if n == 0 or c == 0:  # base case, jak capacity jest zero, lub liczba elementow ktore zostaly do sprawdzenia to zero
-        return 0
-    if t[n][c] != -1:  # sprawdzanie czy jest już taki subproblem rozwiązany
-        return t[n][c]
-    if W[n - 1] <= c:  # jesli sie miesci w plecaku , to sprawdzamy rekurencyjnie max z, uwzgledn. i nie
-        t[n][c] = max(V[n - 1] + dyAlgorithm(c - W[n - 1], n - 1, W, V), dyAlgorithm(c, n - 1, W, V))
-        return t[n][c]
-    elif W[n - 1] > c:  # jak waga jest większa niż capacity to pomijamy
-        t[n][c] = dyAlgorithm(c, n - 1, W, V)
-        return t[n][c]
+# dynamiczny
+def dyAlgorithm(c, n, W, V, t, total_weight):
+    if n == 0 or c == 0:  # base case, jeśli pojemność jest zero lub liczba elementów do sprawdzenia to zero
+        return 0, total_weight, []
+    if t[n][c] != -1:  # sprawdzenie, czy subproblem został już rozwiązany
+        return t[n][c], total_weight, []
+    if W[
+        n - 1] <= c:  # jeśli mieści się w plecaku, sprawdzamy rekurencyjnie maksimum uwzględniając i nieuwzględniając go
+        include_val, include_weight, include_items = dyAlgorithm(c - W[n - 1], n - 1, W, V, t, total_weight + W[n - 1])
+        exclude_val, exclude_weight, exclude_items = dyAlgorithm(c, n - 1, W, V, t, total_weight)
+        if include_val + V[n - 1] > exclude_val:
+            t[n][c] = include_val + V[n - 1]
+            include_items.append(n - 1)
+            return t[n][c], include_weight, include_items
+        else:
+            t[n][c] = exclude_val
+            return t[n][c], exclude_weight, exclude_items
+    elif W[n - 1] > c:  # jeśli waga jest większa niż pojemność, pomijamy ten przedmiot
+        t[n][c], total_weight, exclude_items = dyAlgorithm(c, n - 1, W, V, t, total_weight)
+        return t[n][c], total_weight, exclude_items
 
 
-def sort_lists_by_ratio(V, W):
-    ratios = [v / w for v, w in zip(V, W)]
-    sorted_indices = sorted(range(len(ratios)), key=lambda k: ratios[k], reverse=True)
-    sorted_V = [V[i] for i in sorted_indices]
-    sorted_W = [W[i] for i in sorted_indices]
-    return sorted_V, sorted_W
+def add_item_indices(items, indices):
+    items_with_indices = []
+    for item, index in zip(items, indices):
+        items_with_indices.append((item, index))
+    return items_with_indices
 
 
+def sort_lists_by_ratio(items):
+    sorted_items = sorted(items, key=lambda x: x[0] / W[x[1]], reverse=True)
+    sorted_V = [item[0] for item in sorted_items]
+    sorted_indices = [item[1] for item in sorted_items]
+    return sorted_V, sorted_indices
+
+
+# zachłanny
 def greedy(n, c, W, V):
     value = 0
+    total_weight = 0
+    items_in_bag = []
     i = 0
     while c > 0 and i < n:
         if W[i] <= c:
             value += V[i]
+            total_weight += W[i]
+            items_in_bag.append(sorted_indices[i])
             c -= W[i]
         i += 1
 
-    return value
+    return value, total_weight, items_in_bag
 
 
 print("Algorytm programowania dynamicznego")
 t = [[-1 for i in range(b + 1)] for j in range(n + 1)]
 input()
-print(dyAlgorithm(b, n, W, V))
+max_value, total_weight, selected_items = dyAlgorithm(b, n, W, V, t, 0)
+print("Elementy wybrane do plecaka:", [i for i in selected_items])
+print("Sumaryczny rozmiar:", total_weight)
+print("Wartość:", max_value)
 input()
 print("Algorytm zachłanny")
 input()
-sorted_V, sorted_W = sort_lists_by_ratio(V, W)
-print(greedy(n, b, sorted_W, sorted_V))
+items_with_indices = add_item_indices(V, list(range(n)))
+sorted_V, sorted_indices = sort_lists_by_ratio(items_with_indices)
+sorted_W = [W[i] for i in sorted_indices]
+value, total_weight, items_in_bag = greedy(n, b, sorted_W, sorted_V)
+print("Elementy wybrane do plecaka:", items_in_bag)
+print("Sumaryczny rozmiar:", total_weight)
+print("Wartość:", value)
 input()
 print("Algorytm siłowy")
 input()
 res = bfAlgorithm(n, b, d)
-print("Elementy wybrane do plecaka", res[0])
-print("Sumaryczny rozmiar", res[1])
-print("Wartość", res[2])
+print("Elementy wybrane do plecaka:", res[0])
+print("Sumaryczny rozmiar:", res[1])
+print("Wartość:", res[2])
 input()
 print("Koniec programu")
